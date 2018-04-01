253ce62eacd90c994f04c382f26d17e74f13fab4
Anand Amitash <amitas_vv@yahoo.com>
2018-04-01 13:21:23 +0530

    added Invoker

--- CommandDesignPattern/Program.cs
+++ CommandDesignPattern/Program.cs
@@ -11,27 +11,32 @@ namespace CommandDesignPattern
             //One interface to andle all, it means one remote can handle all.
             //Extensible ,align to S and O of solid principle.
             ICommand switchOnCommand = new LightsOn(light);
-            switchOnCommand.Execute();
+            RemoteButton remoteButton = new RemoteButton(switchOnCommand);
+            remoteButton.ButtonPressed();
             ICommand dimUp = new LightsDimUp(light);
-            dimUp.Execute();
-            dimUp.Execute();
-            dimUp.Execute();
-            dimUp.UnDo();
-            dimUp.UnDo();
-            dimUp.UnDo();
-            switchOnCommand.UnDo();
+            RemoteButton remoteButtonDimUp = new RemoteButton(dimUp);
+            remoteButtonDimUp.ButtonPressed();
+            remoteButtonDimUp.ButtonPressed();
+            remoteButtonDimUp.ButtonPressed();
+            remoteButtonDimUp.Back();
+            remoteButtonDimUp.Back();
+            remoteButtonDimUp.Back();
+            remoteButton.Back();
 
             var fan = new Fan();
             switchOnCommand = new FansOn(fan);
-            switchOnCommand.Execute();
+            remoteButton = new RemoteButton(switchOnCommand);
+            remoteButton.ButtonPressed();
             ICommand speedUp = new FansSpeedUp(fan);
-            speedUp.Execute();
-            speedUp.Execute();
-            speedUp.Execute();
-            speedUp.UnDo();
-            speedUp.UnDo();
-            speedUp.UnDo();
-            switchOnCommand.UnDo();
+            RemoteButton remoteButtonSpeedUp = new RemoteButton(speedUp);
+            remoteButtonSpeedUp.ButtonPressed();
+            remoteButtonSpeedUp.ButtonPressed();
+            remoteButtonSpeedUp.ButtonPressed();
+            remoteButtonSpeedUp.Back();
+            remoteButtonSpeedUp.Back();
+            remoteButtonSpeedUp.Back();
+            remoteButtonSpeedUp.Back();
+            remoteButton.Back();
         }
     }
 }
